08b372af0a5ec34b3e5ef9020cf1850a05efba00
RichieOgbolu <richieogbolu@gmail.com>
2022-03-12 11:49:45 -0800

    A program that displays the alphabet except letter q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (alpha != 'q' && alpha != 'e')
+		{
+			putchar(alpha);
+			alpha++;
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
