83c1e3654d2f78620645ccaea49d2b468a80e3e1
RichieOgbolu <richieogbolu@gmail.com>
2022-03-15 06:50:09 -0700

    Updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,12 @@
  * main - Entry point
  * Description: Prints putchar
  * Return: Always 0 (Success)
- * @c: character to be displayed
 */
 
 int main(void)
 {
 	char alpha[] = "_putchar\n";
 
-	_putchar(alpha);
+	_putchar(alpha[]);
 	return (0);
 }
