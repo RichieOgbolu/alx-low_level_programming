1cbe0659b7b89dbf868d32b7fb6183b44549fd73
RichieOgbolu <richieogbolu@gmail.com>
2022-03-15 15:40:23 -0700

    Updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - contains the command to be executed
+ * @x: character to be executed
+ * @y: character to be executed
+ * Return: return result
+ */
+
+int add(int x, int y)
+{
+	result = x + y;
+	return (result);
+}
+
