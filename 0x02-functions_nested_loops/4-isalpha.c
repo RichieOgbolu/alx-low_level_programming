7576cbcf26b962414daa6faff257ed0f155c481f
RichieOgbolu <richieogbolu@gmail.com>
2022-03-15 11:13:13 -0700

    Updated

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - contains the command to be executed
+ * @c: character to be executed
+ * Return: return 1 if chareacter is a letter and 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'A'))
+		return (1);
+	else
+		return (0);
+}
