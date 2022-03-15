7f4958f771bfcfc4aa4df166b010bdc3bd5d400c
RichieOgbolu <richieogbolu@gmail.com>
2022-03-15 05:48:34 -0700

    A program that displays putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Description: Prints putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar("putchar");
+	_putchar('\n');
+	return (0);
+}
