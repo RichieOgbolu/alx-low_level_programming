88ac9512438f413ed33e0b29b17aa485439d3dff
RichieOgbolu <richieogbolu@gmail.com>
2022-03-15 08:15:20 -0700

    Updated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - These contains the command to be executed
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
