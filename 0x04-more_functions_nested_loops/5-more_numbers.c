e7fc5d5cd3b7aaecc5823062ea81541b8687bfac
RichieOgbolu <richieogbolu@gmail.com>
2022-03-17 10:52:45 -0700

    A program that prints a set of numbers multiple times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - contains the command to be executed
+ * Return: returns void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	while (j < 10)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}
+	_putchar('\n');
+}
