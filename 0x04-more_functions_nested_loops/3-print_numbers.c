754aea8dfcc0e069381c2cb8ccbe6fc501814dd5
RichieOgbolu <richieogbolu@gmail.com>
2022-03-17 10:19:48 -0700

    A program that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - contains command to be executed
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
